c75a3f97c016a50adb792655b76cd7e002162d48
Furkan <furkan6153@gmail.com>
2026-02-21 19:35:18 +0300

    fix-base-path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/social-media-dashboard/",
+  base: "/Social-Media-Dashboard/",
   plugins: [react()],
   build: {
     outDir: "docs",
